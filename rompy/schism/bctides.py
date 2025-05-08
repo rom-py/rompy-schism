88d903d2d47934a4e22aa4ee3f41b2fa244b7ff7
Tom Durrant <t.durrant@oceanum.science>
2025-05-08 15:27:52 +1000

    Removed unncessary additional lines from bctides

--- rompy/schism/bctides.py
+++ rompy/schism/bctides.py
@@ -1010,9 +1010,9 @@ class Bctides:
                         for n in range(num_nodes):
                             f.write("0.0 0.0 0.0 0.0\n")
 
-            # Add remaining sections
-            f.write("0 !ncbn: total # of flow bnd segments with discharge\n")
-            f.write("0 !nfluxf: total # of flux boundary segments\n")
+            # # Add remaining sections
+            # f.write("0 !ncbn: total # of flow bnd segments with discharge\n")
+            # f.write("0 !nfluxf: total # of flux boundary segments\n")
 
         logger.info(f"Successfully wrote bctides.in to {output_file}")
         return output_file
